cd92afb879c8ea3e6c816c8fcc3775653caa3a34
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-11-17 13:08:13 +0000

    Add ventana usage

--- packages/crm/create-contact-from-email/index.ts
+++ packages/crm/create-contact-from-email/index.ts
@@ -1,7 +1,29 @@
-type HandlerOptions = {};
+import Ventana from "@sugarcrm/ventana";
 
-const handler = (handlerOptions: HandlerOptions) => {
-  console.log(handlerOptions);
+const SugarApi = Ventana.getInstance({
+  serverUrl: process.env.serverUrl,
+  platform: process.env.platform,
+  timeout: process.env.serverTimeout,
+  clientID: process.env.clientID,
+});
+
+type HandlerOptions = {
+  __ow_method: "get" | "post";
+};
+
+type HandlerResult = {
+  statusCode?: 404;
+  body?: string;
+};
+
+const handler = (handlerOptions: HandlerOptions): HandlerResult => {
+  if (handlerOptions.__ow_method !== "post") {
+    return { statusCode: 404 };
+  }
+
+  const result = SugarApi.getMetadata();
+
+  console.log(result, handlerOptions);
 
   return { body: "Welcome" };
 };
