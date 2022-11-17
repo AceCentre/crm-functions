994c233a2740c71112723340bc881bedb45eb124
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-11-17 12:22:25 +0000

    Call a shared function

--- packages/crm/create-contact-from-email/index.ts
+++ packages/crm/create-contact-from-email/index.ts
@@ -1,6 +1,8 @@
+import { sharedFunction } from "../../../src/share-lib";
+
 const handler = (...allArgs: any) => {
   console.log(allArgs);
-
+  sharedFunction();
   return { body: "Welcome" };
 };
 
