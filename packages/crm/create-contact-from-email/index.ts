9e2a6670d77b274e3d800f077fb1f1d8d6ac2e8a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-11-17 13:19:13 +0000

    Try with a serverURL

--- packages/crm/create-contact-from-email/index.ts
+++ packages/crm/create-contact-from-email/index.ts
@@ -1,11 +1,13 @@
 import Ventana from "@sugarcrm/ventana";
 
 const SugarApi = Ventana.getInstance({
-  serverUrl: process.env.serverUrl,
-  platform: process.env.platform,
-  timeout: process.env.serverTimeout,
-  clientID: process.env.clientID,
+  serverUrl: process.env.SERVER_URL,
+  // platform: process.env.PLATFORM,
+  // timeout: process.env.SERVER_TIMEOUT,
+  // clientID: process.env.CLIENT_ID,
 });
+
+console.log(process.env);
 
 type HandlerOptions = {
   __ow_method: "get" | "post";
