9c79e278fe50df2136a7141af15eca134743c0db
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-11-21 16:27:42 +0000

    Add more checks

--- packages/crm/create-contact-from-email/index.ts
+++ packages/crm/create-contact-from-email/index.ts
@@ -1,5 +1,7 @@
 type HandlerOptions = {
-  __ow_method: "get" | "post";
+  __ow_method?: "get" | "post";
+  __ow_path?: string;
+  __ow_headers?: {};
 };
 
 type HandlerResult = {
@@ -7,10 +9,20 @@ type HandlerResult = {
   body?: string;
 };
 
-const handler = (handlerOptions: HandlerOptions): HandlerResult => {
+class SugarCrmService {
+  constructor() {}
+}
+
+export const handler = (handlerOptions: HandlerOptions): HandlerResult => {
   if (handlerOptions.__ow_method !== "post") {
     return { statusCode: 404 };
   }
+
+  if (handlerOptions.__ow_path !== "") {
+    return { statusCode: 404 };
+  }
+
+  const crmService = new SugarCrmService();
 
   console.log(handlerOptions);
 
