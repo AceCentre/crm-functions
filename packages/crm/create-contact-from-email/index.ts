612f9f5732968f4cfb714e513129d0c971e4eb3c
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-11-21 16:48:27 +0000

    Try good error handling

--- packages/crm/create-contact-from-email/index.ts
+++ packages/crm/create-contact-from-email/index.ts
@@ -1,32 +1,58 @@
-type HandlerOptions = {
-  __ow_method?: "get" | "post";
+type GenericHandlerOptions = {
+  __ow_method?: string;
   __ow_path?: string;
   __ow_headers?: {};
 };
 
-type HandlerResult = {
-  statusCode?: 404;
-  body?: string;
+type GenericHandlerResult = {
+  statusCode?: number;
+  body?: { reason?: string; message?: string };
 };
 
 class SugarCrmService {
   constructor() {}
 }
 
-export const handler = (handlerOptions: HandlerOptions): HandlerResult => {
-  if (handlerOptions.__ow_method !== "post") {
-    return { statusCode: 404 };
+const ALLOWED_PATHS = ["", "/"];
+
+type CreateContactHandlerOptions = {
+  email?: string;
+} & GenericHandlerOptions;
+
+type CreateContactHandlerResult = {} & GenericHandlerResult;
+
+export const handler = (
+  handlerOptions: CreateContactHandlerOptions
+): CreateContactHandlerResult => {
+  if (
+    handlerOptions.__ow_method === undefined ||
+    handlerOptions.__ow_method.toLowerCase() !== "post"
+  ) {
+    return {
+      statusCode: 404,
+      body: { reason: "Endpoint only accepts POST requests" },
+    };
   }
 
-  if (handlerOptions.__ow_path !== "") {
-    return { statusCode: 404 };
+  if (
+    handlerOptions.__ow_path === undefined ||
+    !ALLOWED_PATHS.includes(handlerOptions.__ow_path)
+  ) {
+    return {
+      statusCode: 404,
+      body: { reason: "Endpoint only accepts requests to /" },
+    };
+  }
+
+  if (!handlerOptions.email) {
+    return { statusCode: 400, body: { reason: "No email provided" } };
   }
 
   const crmService = new SugarCrmService();
 
   console.log(handlerOptions);
 
-  return { body: "Welcome" };
+  return { body: { message: "Welcome" } };
 };
 
 export const main = handler;
--- packages/crm/create-contact-from-email/test/handler.spec.ts
+++ packages/crm/create-contact-from-email/test/handler.spec.ts
@@ -1,11 +1,63 @@
 import { main } from "../index";
 
-test("Forever passing test", () => {
-  expect(true).toEqual(true);
+describe("Check suite is working", () => {
+  test("Forever passing test", () => {
+    expect(true).toEqual(true);
+  });
 });
 
-test("Returns a 404 when no method is given", () => {
-  const result = main({});
+describe("Validate Request", () => {
+  test("Returns a 404 when no method is given", () => {
+    const result = main({ __ow_path: "" });
 
-  expect(result).toEqual({ statusCode: 404 });
+    expect(result).toEqual({
+      statusCode: 404,
+      body: { reason: "Endpoint only accepts POST requests" },
+    });
+  });
+
+  test("Returns a 404 when a get is sent", () => {
+    const result = main({ __ow_path: "", __ow_method: "get" });
+
+    expect(result).toEqual({
+      statusCode: 404,
+      body: { reason: "Endpoint only accepts POST requests" },
+    });
+  });
+
+  test("Returns a 404 when a made up method is sent", () => {
+    const result = main({ __ow_path: "", __ow_method: "not real" });
+
+    expect(result).toEqual({
+      statusCode: 404,
+      body: { reason: "Endpoint only accepts POST requests" },
+    });
+  });
+
+  test("Returns a 404 when no path is given", () => {
+    const result = main({ __ow_method: "post" });
+
+    expect(result).toEqual({
+      statusCode: 404,
+      body: { reason: "Endpoint only accepts requests to /" },
+    });
+  });
+
+  test("Returns a 404 when another path is given", () => {
+    const result = main({ __ow_method: "post", __ow_path: "/this-is-a-path" });
+
+    expect(result).toEqual({
+      statusCode: 404,
+      body: { reason: "Endpoint only accepts requests to /" },
+    });
+  });
+
+  test("Returns a bad request if no email is given", () => {
+    const result = main({ __ow_method: "post", __ow_path: "" });
+
+    expect(result).toEqual({
+      statusCode: 400,
+      body: { reason: "No email provided" },
+    });
+  });
 });
