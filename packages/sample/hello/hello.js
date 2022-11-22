78bb554b525052d1fda2d2e21e84709088a5724a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-11-22 15:19:14 +0000

    Remove sample

--- packages/sample/hello/hello.js
+++
@@ -1,8 +0,0 @@
-function main(args, ...rest) {
-  let name = args.name || "stranger";
-  let greeting = "Hello " + name + "!";
-  console.log(greeting, args, rest);
-  return { body: greeting };
-}
-
-exports.main = main;
