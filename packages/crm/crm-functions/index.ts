cbd6456c8f8a83567dafeb80728d6b949db6250c
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-12-12 15:12:55 +0000

    Refactor handler

--- packages/crm/crm-functions/index.ts
+++ packages/crm/crm-functions/index.ts
@@ -1,61 +1,27 @@
-import { CreateContactHandlerOptions, SlackService } from "./slack-service";
-import { NewContact, SugarService, UpdateContact } from "./sugar-service";
-
-type GenericHandlerResult = {
-  statusCode?: number;
-  body?: string;
-};
-
-const ALLOWED_PATHS = ["", "/"];
-
-type CreateContactHandlerResult = {} & GenericHandlerResult;
-
-const slugify = (input: string): string =>
-  input
-    .toLowerCase()
-    .split(" ")
-    .map((x) => x.trim())
-    .join("-");
+import { addToNewsletter } from "./add-to-newsletter";
+import { SlackService } from "./slack-service";
+import { SugarService } from "./sugar-service";
+import { HandlerInput, HandlerResult } from "./types";
+import { validateRequestParameters } from "./validate-request";
 
 export const handler = async (
-  handlerOptions: CreateContactHandlerOptions
-): Promise<CreateContactHandlerResult> => {
+  handlerOptions: HandlerInput
+): Promise<HandlerResult> => {
   console.log("====== INPUT =======");
   console.log({ handlerOptions });
   console.log("");
 
-  if (
-    handlerOptions.__ow_method === undefined ||
-    handlerOptions.__ow_method.toLowerCase() !== "post"
-  ) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({ reason: "Endpoint only accepts POST requests" }),
-    };
-  }
+  const { valid, reason } = validateRequestParameters(handlerOptions);
 
-  if (
-    handlerOptions.__ow_path === undefined ||
-    !ALLOWED_PATHS.includes(handlerOptions.__ow_path)
-  ) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({ reason: "Endpoint only accepts requests to /" }),
-    };
-  }
-
-  if (!handlerOptions.email) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ reason: "No email provided" }),
-    };
+  if (!valid) {
+    return { statusCode: 404, body: JSON.stringify({ reason }) };
   }
 
   const crmService = new SugarService();
-  let slackService = null;
+  const slackService = new SlackService(handlerOptions);
 
   try {
-    slackService = new SlackService(handlerOptions);
+    slackService.connect();
   } catch (err) {
     console.log("Failed to connect to slack");
     console.log(err);
@@ -66,147 +32,21 @@ export const handler = async (
     };
   }
 
-  try {
-    await crmService.authenticate();
-  } catch (error) {
-    console.log("An error occurred whilst authenticating to SugarCRM");
-    console.log(error);
+  if (handlerOptions.method === "add-to-newsletter") {
+    return await addToNewsletter(handlerOptions, crmService, slackService);
+  }
 
-    slackService.sendError(
-      "An error occurred whilst authenticating to SugarCRM"
-    );
-
+  if (handlerOptions.method === "add-to-course") {
     return {
-      statusCode: 500,
-      body: JSON.stringify({ reason: "Failed to authenticate to SugarCRM." }),
+      statusCode: 404,
+      body: JSON.stringify({ reason: "Not yet implemented" }),
     };
   }
 
-  let listOfExistingContacts = [];
-  try {
-    listOfExistingContacts = await crmService.getContactsByEmail(
-      handlerOptions.email
-    );
-  } catch (error) {
-    console.log("An error occurred whilst trying to get contacts by email");
-
-    slackService.sendError(
-      "An error occurred whilst trying to get contacts by email"
-    );
-
-    console.log(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ reason: "Failed to get contacts by email." }),
-    };
-  }
-
-  if (listOfExistingContacts.length === 0) {
-    console.log("No existing contacts found for:", handlerOptions.email);
-
-    try {
-      let unsavedContact: NewContact = {
-        email: handlerOptions.email,
-        receivesNewsletter: true,
-        firstName: handlerOptions.email,
-        lastName: "Unknown",
-      };
-
-      if (handlerOptions.location) {
-        unsavedContact.location = slugify(handlerOptions.location);
-      }
-
-      if (handlerOptions.firstName) {
-        unsavedContact.firstName = handlerOptions.firstName;
-      }
-
-      if (handlerOptions.lastName) {
-        unsavedContact.lastName = handlerOptions.lastName;
-      }
-
-      const newContact = await crmService.createNewContact(unsavedContact);
-      console.log("Successfully created new contact", newContact.id);
-    } catch (error) {
-      console.log(
-        `An error occurred whilst trying to create a new contact for: ${handlerOptions.email}`
-      );
-      slackService.sendError(
-        `An error occurred whilst trying to create a new contact for: ${handlerOptions.email}`
-      );
-      console.log(error);
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ reason: "Failed to create a new contact." }),
-      };
-    }
-
-    slackService.sendSuccess(`Create a new contact for email.`);
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Create a new contact for email." }),
-    };
-  } else {
-    console.log(
-      `Found ${listOfExistingContacts.length} contact(s) for email ${handlerOptions.email}`
-    );
-
-    for (const currentContact of listOfExistingContacts) {
-      console.log("Opting into mailing for contact:", currentContact.id);
-
-      try {
-        let updateContact: UpdateContact = {
-          id: currentContact.id,
-          receivesNewsletter: true,
-        };
-
-        if (!!currentContact.location == false && handlerOptions.location) {
-          updateContact.location = slugify(handlerOptions.location);
-        }
-
-        if (
-          currentContact.lastName.toLowerCase() === "unknown" &&
-          handlerOptions.lastName
-        ) {
-          updateContact.lastName = handlerOptions.lastName;
-        }
-
-        if (
-          currentContact.firstName.toLowerCase() ===
-            currentContact.email.toLowerCase() &&
-          handlerOptions.firstName
-        ) {
-          updateContact.firstName = handlerOptions.firstName;
-        }
-
-        await crmService.updateContact(updateContact);
-      } catch (error) {
-        console.log(
-          `An error occurred whilst trying to opt ${currentContact.id} into mailing`
-        );
-
-        slackService.sendError(
-          `An error occurred whilst trying to opt ${currentContact.id} into mailing`
-        );
-        console.log(error);
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ reason: "Failed to opt user into mailing." }),
-        };
-      }
-    }
-
-    slackService.sendSuccess(
-      `Updated ${listOfExistingContacts.length} existing contact`
-    );
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: `Updated ${listOfExistingContacts.length} existing contact`,
-      }),
-    };
-  }
+  return {
+    statusCode: 404,
+    body: JSON.stringify({ reason: "No method supplied" }),
+  };
 };
 
 export const main = handler;
