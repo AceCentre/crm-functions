88c3eadd26531ceb665f3a2cc514437799c45983
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-12-13 15:48:02 +0000

    Add example

---
+++ packages/crm/crm-functions/example.ts
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv";
+import { SugarService } from "./sugar-service";
+dotenv.config();
+
+(async () => {
+  const crm = new SugarService({
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+  });
+  await crm.authenticate();
+
+  const events = await crm.getAllEvents();
+  const contacts = await crm.getContactsByEmail("cypress@gavinhenderson.co.uk");
+
+  const event = events[0];
+  const contact = contacts[0];
+
+  const ea = await crm.getEventAttendances(contact, event);
+
+  const newAttendance = await crm.createEventAttendance(event, contact);
+
+  const again = await crm.getEventAttendances(contact, event);
+
+  console.log({ event, contact, ea, newAttendance, again });
+})();
